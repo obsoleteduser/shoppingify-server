473d7b44e6cc1cc53f0a591ca17886982e627101
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-10 21:29:12 +0400

    Shoplist config

--- routes/shopListRoute.js
+++ routes/shopListRoute.js
@@ -5,7 +5,7 @@ const shopListController = require("../controllers/shopListController");
 const shopListRoute = Router()
 
 shopListRoute.post('/shoplist', shopListController.setList)
-shopListRoute.get('/shopList', shopListController.getLists)
+shopListRoute.get('/shoplist', shopListController.getLists)
 shopListRoute.get('/shoplist', shopListController.getWaitingList)
 
 
