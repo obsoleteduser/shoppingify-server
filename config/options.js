2ec3b19915762837bbd97f0d93defe430a4d565c
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-12 00:07:01 +0400

    Cors configuration

---
+++ config/options.js
@@ -0,0 +1,6 @@
+const corsOptions = {
+    origin: ['https://shoppingify-by-tahir.netlify.app/', 'https://shoppingify-client-xi.vercel.app', 'http://localhost:5173'],
+    optionsSuccessStatus: 200
+  }
+  
+export default corsOptions
