1383426005afbda341ee9206a53419949a33d721
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-11 23:48:02 +0400

    #

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const { default: helmet } = require('helmet')
 const bodyParser = require('body-parser')
 
 const app = express()
-// app.use(cors())
-app.options('/user/shoplist', cors(), (req, res) => {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).send();
-  });
+app.use(cors())
+// app.options('/user/shoplist', cors(), (req, res) => {
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.status(200).send();
+//   });
 app.use(express.json())
 app.use('/auth', userRouter)
 app.use('/user', authenticateToken, productRoute)
