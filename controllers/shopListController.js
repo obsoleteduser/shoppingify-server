332732ccda5da366638be25e9e1d357e644414d2
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-12 12:16:55 +0400

    Waiting list endpoint

--- controllers/shopListController.js
+++ controllers/shopListController.js
@@ -2,37 +2,15 @@ const shopListModel = require("../models/shopListModel")
 
 class ShopListController{
 
-    // setList = async (req, res) =>{
-    //     const user = req.user
-    //     const { name, products, status } = req.body
-    //     const shopList = await shopListModel.create({name, products, status, createdBy: user.id})
-    //     res.send(shopList)
-    // }
-
-    setList = async (req, res) => {
-        const user = req.user;
-        const { name, products, status } = req.body;
-      
-        
-        const populatedProducts = await Promise.all(products.map(async (p) => {
-          const populatedProduct = await p.product.populate('product').execPopulate();
-          return {
-            product: populatedProduct,
-            quantity: p.quantity,
-            bought: p.bought,
-          };
-        }));
-      
-       
-        const shopList = await shopListModel.create({ name, products: populatedProducts, status, createdBy: user.id });
-        res.send(shopList);
-      }
-      
-
+    setList = async (req, res) =>{
+        const user = req.user
+        const { name, products, status } = req.body
+        const shopList = await shopListModel.create({name, products, status, createdBy: user.id})
+        res.send(shopList)
+    }
     getWaitingList = async (req, res) =>{
-        const list = await shopListModel.findOne({status: 'waiting'}).populate('products.product')
-        const populatedProducts = await shopListModel.create(list)
-        res.status(200).json(populatedProducts)
+        const list = await shopListModel.findOne({status: 'waiting'}, { sort: { 'createdAt' : -1 }}).populate('products.product')
+        res.status(200).json(list)
     }
     getLists = async (req, res) =>{
         const user = req.user
