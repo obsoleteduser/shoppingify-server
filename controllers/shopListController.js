c26fdd6ae45a36555b0067b08bf474db2af81863
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-13 09:37:44 +0400

    little changes on list contorller

--- controllers/shopListController.js
+++ controllers/shopListController.js
@@ -9,12 +9,12 @@ class ShopListController{
         res.send(shopList)
     }
     getWaitingList = async (req, res) =>{
-        const list = await shopListModel.findOne({status: 'waiting'}, {createdBy: user.id}).sort({ _id : -1 }).populate('products.product')
+        const list = await shopListModel.findOne({status: 'waiting', createdBy: user.id}).sort({ _id : -1 }).populate('products.product')
         res.status(200).json(list)
     }
     getLists = async (req, res) =>{
         const user = req.user
-        const shopLists = await shopListModel.find({createdBy: user.id}, {createdBy: user.id}).populate('products.product')
+        const shopLists = await shopListModel.find({createdBy: user.id, createdBy: user.id}).populate('products.product')
         res.status(200).json(shopLists)
     }
 
