a02d2b803059eeed676b95b4a046eda1eb32c5aa
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-14 22:53:35 +0400

    get list

--- routes/shopListRoute.js
+++ routes/shopListRoute.js
@@ -8,6 +8,6 @@ shopListRoute.post('/shoplist', shopListController.setList)
 shopListRoute.get('/shoplist', shopListController.getLists)
 shopListRoute.get('/shoplistwaiting', shopListController.getWaitingList)
 shopListRoute.put('/updatewaitinglist', shopListController.updateWaitingList)
-shopListRoute.get('/targetlist', shopListController.getList)
+shopListRoute.post('/targetlist', shopListController.getList)
 
 module.exports = shopListRoute
