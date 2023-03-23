58f90528ebd34a9ed1b8a07a5fd0c55437b9cb07
tahirdibirovresearch <tahirdibirovresearch@gmail.com>
2023-03-23 14:29:11 +0300

    #

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,6 +6,6 @@ const productRoute = Router()
 
 productRoute.get('/products', productController.getProducts)
 productRoute.post('/products', productController.setProduct)
-productRoute.delete('/products/:id')
+productRoute.post('/products', prductController.deleteProduct)
 
 module.exports = productRoute
