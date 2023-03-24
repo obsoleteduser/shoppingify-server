0e3a32458720ee08bdfa412c1e4ce17fed5ea1be
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-24 13:22:54 +0400

    #

--- controllers/shopListController.js
+++ controllers/shopListController.js
@@ -1,4 +1,4 @@
-const { productModel } = require("../models/productModel")
+const  productModel  = require("../models/productModel")
 const shopListModel = require("../models/shopListModel")
 
 class ShopListController {
